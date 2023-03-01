fb7e28779b0c0b54fbc8d8ed33c550bab25e0f7b
Sajid Iqbal <amawas1@gmail.com>
2023-03-01 10:57:15 +0500

    WeatherApp cities added

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "Get",
+    headers: {
+        'X-RapidAPI-Key': '6a8b922be9msh857eccdf75011b0p1171efjsnbcf623b0222d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
